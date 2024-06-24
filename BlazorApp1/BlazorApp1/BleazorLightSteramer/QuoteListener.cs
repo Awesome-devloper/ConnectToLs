5dc2b60da77af3ce77caa0ecd2188c3eb9121a31
Awesome <Awesome@DESKTOP-0UOJKJC>
2024-06-24 13:19:29 +0300

    Add project files.

---
+++ BlazorApp1/BlazorApp1/BleazorLightSteramer/QuoteListener.cs
@@ -0,0 +1,80 @@
+﻿using com.lightstreamer.client;
+
+namespace BlazorApp1.BleazorLightSteramer
+{
+	internal class QuoteListener : SubscriptionListener
+	{
+
+		public QuoteListener()
+		{
+
+		}
+
+		void SubscriptionListener.onClearSnapshot(string itemName, int itemPos)
+		{
+			Console.WriteLine("Clear Snapshot for " + itemName + ".");
+		}
+
+		void SubscriptionListener.onCommandSecondLevelItemLostUpdates(int lostUpdates, string key)
+		{
+			Console.WriteLine("Lost Updates for " + key + " (" + lostUpdates + ").");
+		}
+
+		void SubscriptionListener.onCommandSecondLevelSubscriptionError(int code, string message, string key)
+		{
+			Console.WriteLine("Subscription Error for " + key + ": " + message);
+		}
+
+		void SubscriptionListener.onEndOfSnapshot(string itemName, int itemPos)
+		{
+			Console.WriteLine("End of Snapshot for " + itemName + ".");
+		}
+
+		void SubscriptionListener.onItemLostUpdates(string itemName, int itemPos, int lostUpdates)
+		{
+			Console.WriteLine("Lost Updates for " + itemName + " (" + lostUpdates + ").");
+		}
+
+		void SubscriptionListener.onItemUpdate(ItemUpdate itemUpdate)
+		{
+			Console.WriteLine("Item update for " + itemUpdate.ItemName);
+
+			IDictionary<string, string> listc = itemUpdate.ChangedFields;
+			foreach (string value in listc.Values)
+			{
+				Console.WriteLine(" >>>>>>>>>>>>> " + value);
+			}
+		}
+
+		void SubscriptionListener.onListenEnd()
+		{
+			// throw new System.NotImplementedException();
+		}
+
+		void SubscriptionListener.onListenStart()
+		{
+			// throw new System.NotImplementedException();
+		}
+
+		void SubscriptionListener.onRealMaxFrequency(string frequency)
+		{
+			Console.WriteLine("Real frequency: " + frequency + ".");
+		}
+
+		void SubscriptionListener.onSubscription()
+		{
+			Console.WriteLine("Start subscription.");
+		}
+
+		void SubscriptionListener.onSubscriptionError(int code, string message)
+		{
+			Console.WriteLine("Subscription error: " + message);
+		}
+
+		void SubscriptionListener.onUnsubscription()
+		{
+			Console.WriteLine("Stop subscription.");
+		}
+
+	}
+}
---
+++ BlazorApp1/BlazorApp1/BleazorLightSteramer/SystemOutClientListener.cs
@@ -0,0 +1,68 @@
+﻿using com.lightstreamer.client;
+
+namespace BlazorApp1.BleazorLightSteramer
+{
+    public class SystemOutClientListener : ClientListener
+    {
+        private LightstreamerClient client;
+
+        private bool resub = true;
+
+        public SystemOutClientListener(LightstreamerClient client)
+        {
+            this.client = client;
+        }
+
+        public void onListenEnd()
+        {
+        }
+
+        public void onListenStart()
+        {
+        }
+
+        public void onPropertyChange(string property)
+        {
+            Console.WriteLine("Property " + property + " changed: ");
+            if (this.client != null)
+            {
+                if (property.Equals("serverInstanceAddress"))
+                {
+                    Console.WriteLine(this.client.connectionDetails.ServerAddress);
+                }
+                if (property.Equals("sessionId"))
+                {
+                    Console.WriteLine(this.client.connectionDetails.SessionId);
+                }
+
+            }
+        }
+
+        public void onServerError(int errorCode, string errorMessage)
+        {
+            Console.WriteLine("Server Error - " + errorMessage + " - " + errorCode);
+        }
+
+        public void onStatusChange(string status)
+        {
+            Console.WriteLine(" >>>>>>>>>>>>>>>>>> " + status + " - ");
+
+            if ((status.StartsWith("CONNECTED:WS") || status.StartsWith("CONNECTED:HT")) && (resub))
+            {
+                //if (QuickStart.quick_mode == 1)
+                //{
+                //    QuickStart.SubscribeChat();
+                //}
+                //else if (QuickStart.quick_mode == 2)
+                //{
+                //    QuickStart.SubscribeCommand();
+                //}
+                //else if (QuickStart.quick_mode == 0)
+                //{
+                //    QuickStart.SubscribeStocks();
+                //}
+                resub = false;
+            }
+        }
+    }
+}
